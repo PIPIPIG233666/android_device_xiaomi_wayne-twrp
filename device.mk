6f14557c5d6956991becb10d2991437d39cf9b77
Ethan Chen <intervigil@gmail.com>
2018-02-03 14:34:57 -0800

    wayne: Use custom vibrator service
    Change-Id: Icdf633425d9a58346ac71819cc088485ec9f42c6
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    
    # Conflicts:
    #	sepolicy/vendor/file_contexts

--- device.mk
+++ device.mk
@@ -337,8 +337,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service \
+    android.hardware.vibrator@1.1-service.xiaomi_wayne
 
 # Ramdisk
 PRODUCT_PACKAGES += \
