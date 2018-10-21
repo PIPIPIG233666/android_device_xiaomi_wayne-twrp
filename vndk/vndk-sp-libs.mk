371e8baefb43220217ee3971a342cf2605c4d1ac
dianlujitao <dianlujitao@lineageos.org>
2018-10-21 17:54:12 +0800

    wayne: Copy vendor HIDL interface libs to /vendor
     * Move vendor android.hidl.base@1.0 to EXTRA_VENDOR_LIBRARIES as well
       to make code cleaner.
     * Remove android.hidl.manager@1.0-java from product packages as it's
       built by default.
    
    Change-Id: Ia95e09b21ce42137cac9a1dd22f208fd9de9bf20

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -30,4 +30,13 @@ VNDK_SP_LIBRARIES := \
     libunwindstack \
     libutils \
     libutilscallstack \
-    libdexfile
+    libdexfile \
+
+EXTRA_VENDOR_LIBRARIES := \
+    android.hidl.base@1.0 \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.1 \
+    vendor.mokee.livedisplay@2.0 \
+    vendor.mokee.power@1.0 \
+    vendor.mokee.trust@1.0 \
+    vendor.qti.hardware.camera.device@1.0
