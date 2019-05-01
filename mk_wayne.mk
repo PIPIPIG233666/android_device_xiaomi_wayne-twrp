471f0710d4c849f0db2e7029053530c47f2b11ee
PIPIPIG233666 <2212848813@qq.com>
2019-05-01 05:42:50 -0400

    wayne: call MiuiCamera

--- mk_wayne.mk
+++ mk_wayne.mk
@@ -18,6 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/wayne.mk)
 
 # Inherit from wayne device
 $(call inherit-product, device/xiaomi/wayne/device.mk)
